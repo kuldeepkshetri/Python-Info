646a1042039bd9e806f2a271220ef75009ca3e94
kuldeepkshetri <kuldeepkshetri@gmail.com>
2026-05-13 17:27:34 +0530

    1

--- pattern/1to10.py
+++ pattern/1to10.py
@@ -67,3 +67,111 @@ for i in range (l):
         print("*",end='')
 '''               
  
+'''
+12345
+12345
+12345
+12345
+12345
+'''
+
+
+'''
+l=int(input("Enter number :"))
+for i in range (l):
+    print()
+    k=1
+    for j in range (l):
+        print(k,end='')
+        k=k+1
+'''
+
+'''
+
+11111
+22222
+33333
+44444
+55555
+
+'''
+
+
+'''
+l=int(input("Enter number :"))
+for i in range (l):
+    print()
+    for j in range (l):
+        print(i+1,end='')
+
+'''
+
+'''
+1
+00
+111
+0000
+11111
+'''
+
+'''
+l=int(input("Enter number :"))
+for i in range (1,l+1):
+    print()
+    for j in range (i):
+        if i%2==0:
+            print("0",end='')
+        else :
+            print("1",end='')
+'''
+
+'''
+*
+**
+***
+****
+*****
+'''
+
+'''
+l=int(input("Enter number :"))
+for i in range (1,l+1):
+    print()
+    for j in range (i):
+        print("*",end='')
+'''
+
+'''
+1
+12
+123
+1234
+12345
+'''
+'''
+
+l=int(input("Enter number :"))
+for i in range (1,l+1):
+    print()
+    k=1
+    for j in range (i):
+        print(k,end='')
+        k=k+1
+'''
+
+'''
+1
+22
+333
+4444
+55555
+
+'''
+'''
+l=int(input("Enter number :"))
+for i in range (l+1):
+    print()
+    for j in range (i):
+        print(i,end='')
+
+'''
