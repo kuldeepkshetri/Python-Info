9d2e39e4cd5ceccadcc8c961ad6023375423b6ed
kuldeepkshetri <kuldeepkshetri@gmail.com>
2026-05-13 17:10:53 +0530

    1

---
+++ pattern/1to10.py
@@ -0,0 +1,69 @@
+'''
+1) Draw patterns
+*****	
+'''
+
+'''
+l=int(input("Enter number :"))
+
+for i in range (1):
+    print()
+    for j in range (l):
+        print("*",end='')
+'''
+
+'''
+2
+*
+*
+*
+*
+*
+'''
+
+'''
+l=int(input("Enter number :"))
+
+for i in range (l):
+    print("*")
+
+'''
+
+'''
+*
+  *
+    *
+      *
+        *
+        '''
+
+'''
+l=int(input("Enter number :"))
+for i in range (l):
+    print()
+    for j in range (l):
+        if i==j:
+            print("*",end='')
+        else :
+            print(" ",end='')
+'''
+
+
+
+'''
+
+*****
+*****
+*****
+*****
+*****
+
+'''
+'''
+l=int(input("Enter number :"))
+for i in range (l):
+    print()
+    for j in range (l):
+        print("*",end='')
+'''               
+ 
